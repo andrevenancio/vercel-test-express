57ff63ebdd8f1e7a45488ec0a5641c602d40a93a
andrevenancio <info@andrevenancio.com>
2024-07-17 00:47:59 +0100

    test

--- build/server.js
+++ build/server.js
@@ -4,13 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+dotenv_1.default.config();
 app.get("/", (_req, res) => {
-    return res.send("Express Typescript on Vercel.2");
+    return res.send("Express Typescript on Vercel.3");
 });
 app.get("/ping", (_req, res) => {
-    return res.json({ message: "pong 🏓", data: process.env.TEST });
+    return res.json({
+        message: "pong 🏓",
+        test: process.env.TEST || "not defined",
+    });
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express, { Request, Response } from "express"
+import dotenv from "dotenv"
 
 const app = express()
 const port = process.env.PORT || 8080
 
+dotenv.config()
+
 app.get("/", (_req: Request, res: Response) => {
-  return res.send("Express Typescript on Vercel.2")
+  return res.send("Express Typescript on Vercel.3")
 })
 
 app.get("/ping", (_req: Request, res: Response) => {
-  return res.json({ message: "pong 🏓", data: process.env.TEST })
+  return res.json({
+    message: "pong 🏓",
+    test: process.env.TEST || "not defined",
+  })
 })
 
 app.listen(port, () => {
